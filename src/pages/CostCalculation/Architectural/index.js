e279be8d649f5892ef8dd3671358e83ee36c662a
Shiv Dadhania <shivdadhaniya105@gmail.com>
2023-10-18 13:15:44 +0530

    architectural tab ui complete

--- src/pages/CostCalculation/Architectural/index.js
+++ src/pages/CostCalculation/Architectural/index.js
@@ -24,29 +24,100 @@ class Architectural extends Component {
           </button>
         </div>
         <div className="tab-content">
-        <div className="card tabs-charts ">
-          <div className="compute-contain">
-            <div className="compute-total-cost">
-              <span className="d-block total">Total cost</span>
-              <span className="d-block number">$12,875</span>
-              <span className="d-block compair">
-                Compared to $21,490 last year
-              </span>
+          <div className="tab-pane active">
+            <div className="card tabs-charts ">
+              <div className="compute-contain">
+                <div className="compute-total-cost">
+                  <span className="d-block total">Total cost</span>
+                  <span className="d-block number">$12,875</span>
+                  <span className="d-block compair">
+                    Compared to $21,490 last year
+                  </span>
+                </div>
+                <div className="compute-cost">
+                  <span>150 instances</span>
+                  <div className="progress"></div>
+                </div>
+                <div className="compute-cost">
+                  <span>$0.45/hour</span>
+                  <div className="progress"></div>
+                </div>
+                <div className="compute-cost">
+                  <span>$ 600,000 </span>
+                  <div className="progress"></div>
+                </div>
+              </div>
             </div>
-            <div className="compute-cost">
-              <span>150 instances</span>
-              <div className="progress"></div>
+            <div className="apply-box">
+              <div className="top-contain d-flex justify-content-between align-items-center">
+                <span>Container</span>
+                <div className="form-check form-switch d-flex align-items-center">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    role="switch"
+                    id="flexSwitchCheckDefault"
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor="flexSwitchCheckDefault"
+                  >
+                    Apply
+                  </label>
+                </div>
+              </div>
+              <div className="bottom-contain d-flex justify-content-between align-items-center">
+                <span>Cost will Reduce Upto 25%</span>
+                <span className="impact-details">Impact View</span>
+              </div>
             </div>
-            <div className="compute-cost">
-              <span>$0.45/hour</span>
-              <div className="progress"></div>
+            <div className="apply-box">
+              <div className="top-contain d-flex justify-content-between align-items-center">
+                <span>Serverless</span>
+                <div className="form-check form-switch d-flex align-items-center">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    role="switch"
+                    id="flexSwitchCheckDefault"
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor="flexSwitchCheckDefault"
+                  >
+                    Apply
+                  </label>
+                </div>
+              </div>
+              <div className="bottom-contain d-flex justify-content-between align-items-center">
+                <span>Cost will Reduce Upto 20%</span>
+                <span className="impact-details">Impact View</span>
+              </div>
             </div>
-            <div className="compute-cost">
-              <span>$ 600,000 </span>
-              <div className="progress"></div>
+            <div className="apply-box">
+              <div className="top-contain d-flex justify-content-between align-items-center">
+                <span>Top 10 Easy</span>
+                <div className="form-check form-switch d-flex align-items-center">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    role="switch"
+                    id="flexSwitchCheckDefault"
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor="flexSwitchCheckDefault"
+                  >
+                    Apply
+                  </label>
+                </div>
+              </div>
+              <div className="bottom-contain d-flex justify-content-between align-items-center">
+                <span>Cost will Reduce Upto 25%</span>
+                <span className="impact-details">Impact View</span>
+              </div>
             </div>
           </div>
-        </div>
         </div>
       </div>
     );
--- src/pages/CostCalculation/Footer.js
+++ src/pages/CostCalculation/Footer.js
@@ -6,125 +6,134 @@ class Footer extends Component {
     return (
       <div className="footer">
         <div className="w-75 mx-auto">
-        <div className="container-fluid">
-        <div className="menu-list">
-          <div className="row ">
-           
-            <div className="col-md-5 align-items-center">
-              <img src={logo} alt="" />
-              <div className="subscribe-info">
-                <div className="container">
-                  <span>Subscribe to our newsletter</span>
-                  <input
-                    className="email-input"
-                    placeholder="Enter your email"
-                  />
+          <div className="container-fluid">
+            <div className="menu-list">
+              <div className="row ">
+                <div className="col-md-5 align-items-center">
+                  <img src={logo} alt="" />
+                  <div className="subscribe-info">
+                    <div className="container">
+                      <span>Subscribe to our newsletter</span>
+                      <input
+                        className="email-input"
+                        placeholder="Enter your email"
+                      />
 
-                  <button type="button" class="btn btn-primary">
-                    Subscribe
-                  </button>
+                      <button type="button" class="btn btn-primary">
+                        Subscribe
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-2">
+                  <div className="d-block menu">
+                    <label>Pages</label>
+                    <ul>
+                      <li>
+                        <a href="#">Home</a>
+                      </li>
+                      <li>
+                        <a href="#">About</a>
+                      </li>
+                      <li>
+                        <a href="#">Contact</a>
+                      </li>
+                      <li>
+                        <a href="#">Blog</a>
+                      </li>
+                      <li>
+                        <a href="#">Blog post</a>
+                      </li>
+                      <li>
+                        <a href="#">Pricing</a>
+                      </li>
+                      <li>
+                        <a href="#">Pricing single</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="col-md-2">
+                  <div className="d-block menu">
+                    <label></label>
+                    <ul>
+                      <li>
+                        <a href="#">Features</a>
+                      </li>
+                      <li>
+                        <a href="#">Careers</a>
+                      </li>
+                      <li>
+                        <a href="#">Careers single</a>
+                      </li>
+                      <li>
+                        <a href="#">Request a demo</a>
+                      </li>
+                      <li>
+                        <a href="#">Login</a>
+                      </li>
+                      <li>
+                        <a href="#">Sign Up</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="col-3">
+                  <div className="d-block menu">
+                    <label>Utility Pages</label>
+                    <ul>
+                      <li>
+                        <a href="#">Style guide</a>
+                      </li>
+                      <li>
+                        <a href="#">Password protected</a>
+                      </li>
+                      <li>
+                        <a href="#">404 Not found</a>
+                      </li>
+                      <li>
+                        <a href="#">Licenses</a>
+                      </li>
+                      <li>
+                        <a href="#">Changelog</a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="col-md-2">
-              <div className="d-block menu">
-                <label>Pages</label>
+            <div className="copyright-details d-flex justify-content-between align-items-center">
+              <span>
+                Copyright © Product | Designed by <a href="#">Webocean LTD</a> -
+                Powered by <a href="#">Webflow</a>
+              </span>
+
+              <div className="social-icon">
                 <ul>
                   <li>
-                    <a href="#">Home</a>
+                    <a href="#">
+                      <i class="fa-brands fa-facebook-f"></i>
+                    </a>
                   </li>
                   <li>
-                    <a href="#">About</a>
+                    <a href="#">
+                      <i class="fa-brands fa-twitter"></i>
+                    </a>
                   </li>
                   <li>
-                    <a href="#">Contact</a>
+                    <a href="#">
+                      <i class="fa-brands fa-linkedin-in"></i>
+                    </a>
                   </li>
                   <li>
-                    <a href="#">Blog</a>
-                  </li>
-                  <li>
-                    <a href="#">Blog post</a>
-                  </li>
-                  <li>
-                    <a href="#">Pricing</a>
-                  </li>
-                  <li>
-                    <a href="#">Pricing single</a>
+                    <a href="#">
+                      <i class="fa-brands fa-instagram"></i>
+                    </a>
                   </li>
                 </ul>
-              </div>
-            </div>
-            <div className="col-md-2">
-              <div className="d-block menu">
-                <label></label>
-                <ul>
-                  <li>
-                    <a href="#">Features</a>
-                  </li>
-                  <li>
-                    <a href="#">Careers</a>
-                  </li>
-                  <li>
-                    <a href="#">Careers single</a>
-                  </li>
-                  <li>
-                    <a href="#">Request a demo</a>
-                  </li>
-                  <li>
-                    <a href="#">Login</a>
-                  </li>
-                  <li>
-                    <a href="#">Sign Up</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-3">
-              <div className="d-block menu">
-                <label>Utility Pages</label>
-                <ul>
-                  <li>
-                    <a href="#">Style guide</a>
-                  </li>
-                  <li>
-                    <a href="#">Password protected</a>
-                  </li>
-                  <li>
-                    <a href="#">404 Not found</a>
-                  </li>
-                  <li>
-                    <a href="#">Licenses</a>
-                  </li>
-                  <li>
-                    <a href="#">Changelog</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            </div>
-          </div>
-          .
-          <div className="copywrite-details d-flex justify-content-between align-items-center">
-            <span>
-              Copyright © Product | Designed by <a href="#">Webocean LTD</a> -
-              Powered by <a href="#">Webflow</a>
-            </span>
-            <div className="d-flex">
-              <div className="social-icon">
-                <i class="fa-brands fa-facebook-f"></i>
-              </div>
-              <div className="social-icon">
-                <i class="fa-brands fa-twitter"></i>
-              </div>
-              <div className="social-icon">
-                <i class="fa-brands fa-linkedin-in"></i>
-              </div>
-              <div className="social-icon">
-                <i class="fa-brands fa-instagram"></i>
               </div>
             </div>
           </div>
-        </div>
         </div>
       </div>
     );
