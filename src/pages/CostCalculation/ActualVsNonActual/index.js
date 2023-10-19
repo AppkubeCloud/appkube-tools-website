509b6abe81c77d4991b7abd7f84409f645809950
Shiv Dadhania <shivdadhaniya105@gmail.com>
2023-10-19 18:24:33 +0530

    actualVsNonActual page graph added

--- src/pages/CostCalculation/ActualVsNonActual/index.js
+++ src/pages/CostCalculation/ActualVsNonActual/index.js
@@ -1,5 +1,47 @@
 import React, { Component } from "react";
+import { Pie, Bar } from "react-chartjs-2";
+
 class ActualVsNonActual extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        labels: ["Non-Actual", "Actual"],
+        datasets: [
+          {
+            data: [70, 30],
+            backgroundColor: ["#F9D33D", "#53CA43"],
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          legend: {
+            position: "right",
+            labels: {
+              usePointStyle: true,
+              color: "#383874",
+              font: {
+                size: 16,
+              },
+              padding: 40,
+            },
+          },
+        },
+      },
+      barData: {
+        labels: ["Label 1", "Label 2", "Label 3", "Label 4"],
+        datasets: [
+          {
+            label: "Sample Bar Chart",
+            data: [12, 19, 3, 5],
+            backgroundColor: "rgba(75, 192, 192, 0.6",
+          },
+        ],
+      },
+    };
+  }
+
   render() {
     return (
       <div className="optimization-option-tab">
@@ -22,7 +64,52 @@ class ActualVsNonActual extends Component {
             Next
           </button>
         </div>
-      
+        <div className="actual-non-actual-charts">
+          <div className="card charts">
+            <div className="d-flex justify-content-between align-items-start">
+              <div className="d-block">
+                <span className="d-block total-sepnd">Total Spend</span>
+                <span className="d-block number">$100M</span>
+                <span className="d-block text">Before Optimization</span>
+              </div>
+              <div className="form-check form-switch d-flex align-items-center">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  role="switch"
+                  id="flexSwitchCheckDefault"
+                />
+                <label
+                  className="form-check-label"
+                  htmlFor="flexSwitchCheckDefault"
+                >
+                  Apply
+                </label>
+              </div>
+            </div>
+            <div style={{ paddingLeft: "50px" }}>
+              <div className="pie-chart">
+                <Pie data={this.state.data} options={this.state.options} />
+              </div>
+            </div>
+          </div>
+          <div className="card charts">
+            <div style={{ paddingLeft: "50px" }}>
+              <div className="bar-chart">
+                <Bar
+                  data={this.state.barData}
+                  options={{
+                    scales: {
+                      y: {
+                        beginAtZero: true,
+                      },
+                    },
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
